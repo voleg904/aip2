df71967a22677abadd2810602b6a7c1ca8b374da
Oleg <n7o.nto@yandex.ru>
2025-11-01 21:57:13 +0300

    Code layout added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 
-int main(){}
+int **make(int rows,int cols);
+void output(int **mtx);
+void rm(int **mtx);
+
+int main(){
+  int rows = 0, cols = 0;
+  std::cin >> rows >> cols;
+  if (std::cin.fail()){
+    return 1;
+  }
+  int **mtx = nullptr;
+  mtx = make(rows,cols);
+  output(mtx);
+  rm(mtx);
+}
