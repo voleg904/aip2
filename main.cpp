a7da370fd6e0a394ac4d89beb17c3d2eeb92027c
Oleg <n7o.nto@yandex.ru>
2025-11-01 23:07:25 +0300

    Added functions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <new>
 
-int **make(int rows,int cols);
-void output(int **mtx);
-void rm(int **mtx);
+void rm(int **mtx, int r){
+  for (size_t i = 0; i<r; ++i){
+    delete[] mtx[i];
+  }
+  delete[] mtx;
+}
+
+int **make(int r,int c){
+  int **mtx = new int *[r];
+  for (size_t i = 0; i<r; ++i){
+    try{
+      mtx[i] = new int[c];
+    }
+    catch (const std::bad_alloc &){
+      rm(mtx, i);
+      throw;
+    }
+  }
+  return mtx;
+}
+void output(const int *const *mtx, int r, int c){
+  for (size_t i = 0; i < r; ++i){
+    for (size_t j = 0; j < c; ++j){
+      std::cout << mtx[i][j] << ' ';
+    }
+    std::cout << '\n';
+  }
+}
+void input(int **mtx, int r, int c){
+  for (size_t i = 0; i < r; ++i){
+    for (size_t j = 0; j < c; ++j){
+      std::cin >> mtx[i][j];
+    }
+  }
+}
 
 int main(){
   int rows = 0, cols = 0;
@@ -11,7 +44,19 @@ int main(){
     return 1;
   }
   int **mtx = nullptr;
-  mtx = make(rows,cols);
-  output(mtx);
-  rm(mtx);
+  try{
+   mtx = make(rows,cols); 
+  }
+  catch (const std::bad_alloc &){
+    return 2;
+  }
+  input(mtx,rows,cols);
+  if (std::cin.fail()){
+    rm(mtx,rows);
+    return 1;
+  }
+  output(mtx, rows, cols);
+  rm(mtx, cols);
 }
+
+
